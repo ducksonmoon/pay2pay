6e8c65f39c8c07d7036184d898b8df12b6c99e2b
Mehrshad Baqerzadegan <MehrshadBaqerzadegan@gmail.com>
2023-06-15 20:52:20 +0330

    Add postgres

--- services/app/settings.py
+++ services/app/settings.py
@@ -106,9 +106,12 @@ WSGI_APPLICATION = "app.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # }
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
     }
 }
 
